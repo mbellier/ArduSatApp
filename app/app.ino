3bf1f4bf720695d265f78ac3772333122c3ce6ca
mbellier <mbellier@enseirb-matmeca.fr>
2013-12-12 10:49:01 +0100

    Changed location and time

--- app/app.ino
+++ app/app.ino
@@ -1,19 +1,27 @@
 #include "Plan13.h"
 
+// object used to compute the satellite position
 Plan13 p13;
 
 void setup () {
-  p13.setLocation(-64.375, 45.8958, 20); // lon, lat, height
   Serial.begin(38400);
+
+  // set the position of an observer
+  p13.setLocation(0.607696, 44.806265, 20); // lon, lat, height
+  
 }
 
 void loop() { 
-  // year, mount, day, hour, minute, second
-  p13.setTime(2009, 10, 1, 19, 5, 0);
-  // TLE
+  
+  // TODO time
+  // year, mounth, day, hour, minute, second
+  p13.setTime(2013, 12, 8, 13, 37, 42);
+  // TLE (example values)
   p13.setElements(2009, 232.55636497, 98.0531, 238.4104, 83652*1.0e-7, 290.6047,
   68.6188, 14.406497342, -0.00000001, 27022, 180.0); 
+  // compute satellite position
   p13.calculate();
+  // display satellite position (and position from an observer)
   p13.printdata();
   
   delay(2000);
